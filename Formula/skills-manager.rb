670e687e2b6e201051fd96704f7a560fe6924223
Jakub <25692065+jboczek@users.noreply.github.com>
2026-08-29 15:56:37 +0200

    skills-manager 0.1.3 (#9)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/skills-manager.rb
+++ Formula/skills-manager.rb
@@ -4,19 +4,19 @@ class SkillsManager < Formula
 
   def self.configure_architecture
     on_arm do
-      url "https://github.com/jboczek/skills-manager/releases/download/v0.1.0/skills-manager-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "9045e67f1254ef2d55db33e12c16f5c53366bda864b3ed2d69f582c002cc7512"
+      url "https://github.com/jboczek/skills-manager/releases/download/v0.1.3/skills-manager-v0.1.3-aarch64-apple-darwin.tar.gz"
+      sha256 "5f668befe0f742b9306e62f1cd9ee6df3db9d1d153d1c06caf022b3681921b5d"
     end
 
     on_intel do
-      url "https://github.com/jboczek/skills-manager/releases/download/v0.1.0/skills-manager-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "15f7c71c0345aa7803712aff6fd83869269ffbfb9371dee08e47c283f9cbc751"
+      url "https://github.com/jboczek/skills-manager/releases/download/v0.1.3/skills-manager-v0.1.3-x86_64-apple-darwin.tar.gz"
+      sha256 "59ff6078de2baa25ffea2a29ea0858a638f47860bbc26a598a3ebe5cec77635d"
     end
   end
 
   configure_architecture
-  version "0.1.0"
-  stable.version Version.new("0.1.0", detected_from_url: true)
+  version "0.1.3"
+  stable.version Version.new("0.1.3", detected_from_url: true)
   license "MIT"
   depends_on :macos
 
